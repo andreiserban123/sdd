b65deda5ed28de97fa7de38e38004f2f3a6639de
SERBAN EC ANDREI <andyspeed2003@gmail.com>
2024-03-23 15:46:06 +0200

    tema1

---
+++ seminar3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+
+typedef struct student
+{
+    int nrMatricol;
+    char *nume;
+    float medie;
+
+} student;
+
+typedef struct nodls
+{
+    student info;
+    struct nodls *next, *prev;
+} nodls;
+
+nodls *inserare(nodls *cap, nodls **coada, student s)
+{
+    nodls *nou = malloc(sizeof(nodls));
+    nou->info.nume = malloc(strlen(s.nume) + 1);
+    strcpy(nou->info.nume, s.nume);
+    nou->info.medie = s.medie;
+    nou->info.nrMatricol = s.nrMatricol;
+    nou->next = NULL;
+    nou->prev = NULL;
+    if (cap == NULL)
+    {
+        cap = nou;
+        *coada = nou;
+    }
+    else
+    {
+    }
+    return cap;
+}
---
+++ stiva.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+typedef struct nod
+{
+    int inf;
+    struct nod next;
+} nod;
+
+typedef struct stiva
+{
+} stiva;
+
+int main()
+{
+    return 0;
+}
