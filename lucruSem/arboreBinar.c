f6f91c19a6c9964463281a051b5cf65106048c25
SERBAN EC ANDREI <andyspeed2003@gmail.com>
2024-05-04 11:31:09 +0300

    arbore binar

--- lucruSem/arboreBinar.c
+++ lucruSem/arboreBinar.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
 #include <malloc.h>
@@ -8,36 +9,37 @@ typedef struct Cofetarie
     char *nume;
 } Cofetarie;
 
-typedef struct Nod
+typedef struct nod
 {
+    struct nod *stanga;
+    struct nod *dreapta;
     Cofetarie inf;
-    struct Nod *stanga;
-    struct Nod *dreapta;
-} Nod;
+} nod;
 
-void inserareArbore(Cofetarie c, Nod **radacina)
+void inserare(nod **radacina, Cofetarie inf)
 {
-    if (*radacina == NULL)
+    if ((*radacina) == NULL)
     {
-        Nod *nou = malloc(sizeof(Nod));
-        nou->inf = c;
-        nou->dreapta = nou->stanga = NULL;
+        nod *nou = malloc(sizeof(nod));
+        nou->inf = inf;
+        nou->stanga = NULL;
+        nou->dreapta = NULL;
         *radacina = nou;
     }
     else
     {
-        if (c.id > (*radacina)->inf.id)
+        if (inf.id > (*radacina)->inf.id)
         {
-            inserareArbore(c, &(*radacina)->dreapta);
+            inserare(&(*radacina)->dreapta, inf);
         }
         else
         {
-            inserareArbore(c, &(*radacina)->stanga);
+            inserare(&(*radacina)->stanga, inf);
         }
     }
 }
 
-void afisareInOrdine(Nod *radacina)
+void afisareInOrdine(nod *radacina)
 {
     if (radacina != NULL)
     {
@@ -46,7 +48,7 @@ void afisareInOrdine(Nod *radacina)
         afisareInOrdine(radacina->dreapta);
     }
 }
-void afisarePreOrdine(Nod *radacina)
+void afisarePreOrdine(nod *radacina)
 {
     if (radacina)
     {
@@ -56,7 +58,7 @@ void afisarePreOrdine(Nod *radacina)
     }
 }
 
-void afisarePostOrdine(Nod *radacina)
+void afisarePostOrdine(nod *radacina)
 {
     if (radacina)
     {
@@ -66,33 +68,88 @@ void afisarePostOrdine(Nod *radacina)
     }
 }
 
-void dezalocareArbore(Nod **radacina)
+void cautareDupaId(Cofetarie *c, nod *radacina, int idCautat)
+{
+    if (radacina)
+    {
+        if (radacina->inf.id == idCautat)
+            *c = radacina->inf;
+        else
+        {
+            if (idCautat < radacina->inf.id)
+            {
+                cautareDupaId(c, radacina->stanga, idCautat);
+            }
+            else
+            {
+                cautareDupaId(c, radacina->dreapta, idCautat);
+            }
+        }
+    }
+    else
+    {
+        Cofetarie x;
+        x.id = -1;
+        x.nume = malloc(1);
+        strcpy(x.nume, "");
+        *c = x;
+    }
+}
+
+void dezalocareArbore(nod **radacina)
 {
     if (*radacina)
     {
         dezalocareArbore(&(*radacina)->stanga);
         dezalocareArbore(&(*radacina)->dreapta);
         free((*radacina)->inf.nume);
-        free(*radacina);
+        free((*radacina));
     }
     *radacina = NULL;
 }
 
-int main(int argc, char const *argv[])
+int calculInaltime(nod *radacina)
+{
+    if (radacina)
+    {
+        int inaltimeS = calculInaltime(radacina->stanga);
+        int inaltimeD = calculInaltime(radacina->dreapta);
+
+        if (inaltimeD > inaltimeS)
+            return inaltimeD + 1;
+        else
+            return inaltimeS + 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int main()
 {
     FILE *f = fopen("arb.txt", "r");
-    Nod *radacina = NULL;
     Cofetarie c;
+    nod *radacina = NULL;
     while (fscanf(f, "%d", &c.id) == 1)
     {
         char buffer[200];
         fscanf(f, "%s", buffer);
         c.nume = malloc(strlen(buffer) + 1);
         strcpy(c.nume, buffer);
-        inserareArbore(c, &radacina);
+        inserare(&radacina, c);
     }
-    // afisareInOrdine(radacina);
-    afisarePostOrdine(radacina);
+    afisarePreOrdine(radacina);
+    cautareDupaId(&c, radacina, 1000);
+    printf("\nID CAUTAT: %d, nume:%s", c.id, c.nume);
+    if (c.id == -1)
+    {
+        free(c.nume);
+    }
+
+    int inaltime = calculInaltime(radacina);
+    printf("\nInaltimea Arborelului:%d", inaltime);
+
     dezalocareArbore(&radacina);
     return 0;
 }
