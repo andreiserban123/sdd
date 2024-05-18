d4656cec94249fb815e546457e2f2325660ef4b9
SERBAN EC ANDREI <andyspeed2003@gmail.com>
2024-05-18 11:05:37 +0300

    ld

---
+++ lista_de_liste/ldl.c
@@ -0,0 +1,114 @@
+#include <malloc.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct produs
+{
+    int cod;
+    char *denumire;
+    float pret;
+} produs;
+
+typedef struct nodLS
+{
+    produs inf;
+    struct nodLS *next;
+} nodLS;
+
+typedef struct nodLP
+{
+    nodLS *inf;
+    struct nodLP *next;
+
+} nodLP;
+
+nodLS *inserareLS(nodLS *cap, produs p)
+{
+    nodLS *nou = (nodLS *)malloc(sizeof(nodLS));
+    nou->inf = p;
+    nou->next = NULL;
+    if (cap == NULL)
+    {
+        cap = nou;
+    }
+    else
+    {
+        nodLS *temp = cap;
+        while (temp->next)
+        {
+            temp = temp->next;
+        }
+        temp->next = nou;
+    }
+    return cap;
+}
+
+void traversareLS(nodLS *cap)
+{
+    nodLS *temp = cap;
+    while (temp)
+    {
+        printf("Cod: %d, Denumire: %s, Pret: %5.2f\n", temp->inf.cod, temp->inf.denumire, temp->inf.pret);
+        temp = temp->next;
+    }
+}
+
+void traversareLP(nodLP *cap)
+{
+    nodLP *temp = cap;
+    int i = 0;
+    while (temp)
+    {
+        printf("Lista %d:\n", i++);
+        traversareLS(temp->inf);
+        temp = temp->next;
+    }
+}
+
+nodLP *inserareLP(nodLP *cap, nodLS *l)
+{
+    nodLP *nou = malloc(sizeof(nodLP));
+    nou->next = NULL;
+    nou->inf = l;
+    if (cap == NULL)
+    {
+        return nou;
+    }
+    nodLP *tmp = cap;
+    while (tmp->next)
+    {
+        tmp = tmp->next;
+    }
+    tmp->next = nou;
+}
+
+int main()
+{
+    nodLP *cap = NULL;
+    FILE *f = fopen("fisier.txt", "r");
+    nodLS *l1 = NULL;
+    nodLS *l2 = NULL;
+    int nrEl;
+    char buffer[100];
+    produs p;
+    fscanf(f, "%d", &nrEl);
+    for (int i = 0; i < nrEl; i++)
+    {
+        fscanf(f, "%d", &p.cod);
+        fscanf(f, "%s", buffer);
+        p.denumire = (char *)malloc((strlen(buffer) + 1) * sizeof(char));
+        strcpy(p.denumire, buffer);
+        fscanf(f, "%f", &p.pret);
+        if (p.cod % 2 == 0)
+            l1 = inserareLS(l1, p);
+        else
+            l2 = inserareLS(l2, p);
+    }
+
+    cap = inserareLP(cap, l1);
+    cap = inserareLP(cap, l2);
+
+    traversareLP(cap);
+
+    return 0;
+}
